b9e2796c7de334dd320732514fa159d4adaa551c
Andrey <andreypomortsev@gmail.com>
2022-12-19 15:18:59 +0500

    requirements update

--- ml.py
+++ ml.py
@@ -6,18 +6,17 @@ from PIL.Image import Image
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-device = "CUDA"
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
+    torch_dtype=torch.float16, 
     revision="fp16",
-    torch_dtype=torch.float16,
     use_auth_token=TOKEN
 )
 
-pipe = pipe.to(device)
+pipe = pipe.to('cuda')
 
-prompt = input()
+prompt = input('WHa ')
 
 image = pipe(prompt).images[0]  
     
