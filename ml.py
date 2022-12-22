b52bcbba3f9e09048ad956f6be83aaa15d3a8b8b
Andrey <andreypomortsev@gmail.com>
2022-12-22 09:46:12 +0500

    Free some GPU RAM

--- ml.py
+++ ml.py
@@ -2,22 +2,26 @@ import os
 from dotenv import load_dotenv
 import torch
 from diffusers import StableDiffusionPipeline
-from PIL.Image import Image
+# from PIL.Image import Image
+import gc
 
+gc.collect()
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
-    torch_dtype=torch.float16, 
-    revision="fp16",
-    use_auth_token=TOKEN
+    torch_dtype = torch.float16,
+    revision = "fp16",
+    use_auth_token = TOKEN
 )
-
+torch.cuda.empty_cache()
+pipe.batch_size = 1
 pipe = pipe.to('cuda')
 
-prompt = input('WHa ')
+prompt = "A handsome bunny abasks on the beach"
 
-image = pipe(prompt).images[0]  
+image = pipe(f"{prompt}").images[0]  
     
 image.save(f"{prompt}.png")
+torch.cuda.empty_cache()
