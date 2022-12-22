df98920363940710b6251849331afa75d510a2e4
Andrey <andreypomortsev@gmail.com>
2022-12-22 09:49:49 +0500

    Add function to clean trash

--- ml.py
+++ ml.py
@@ -5,9 +5,14 @@ from diffusers import StableDiffusionPipeline
 # from PIL.Image import Image
 import gc
 
-gc.collect()
+def ram_cleaning() -> None:
+    gc.collect()
+    torch.cuda.empty_cache()
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+
+ram_cleaning()
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
@@ -15,13 +20,11 @@ pipe = StableDiffusionPipeline.from_pretrained(
     revision = "fp16",
     use_auth_token = TOKEN
 )
-torch.cuda.empty_cache()
-pipe.batch_size = 1
+
 pipe = pipe.to('cuda')
 
 prompt = "A handsome bunny abasks on the beach"
-
 image = pipe(f"{prompt}").images[0]  
-    
 image.save(f"{prompt}.png")
-torch.cuda.empty_cache()
+
+ram_cleaning()
