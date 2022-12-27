918265ba219a84e8bc2227f7ef8af9df756df1c9
Andrey <andreypomortsev@gmail.com>
2022-12-27 15:35:13 +0500

    Added a function obtain_image

--- ml.py
+++ ml.py
@@ -2,29 +2,35 @@ import os
 from dotenv import load_dotenv
 import torch
 from diffusers import StableDiffusionPipeline
-# from PIL.Image import Image
-import gc
-
-def ram_cleaning() -> None:
-    gc.collect()
-    torch.cuda.empty_cache()
+from PIL.Image import Image
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
-ram_cleaning()
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
     torch_dtype = torch.float16,
     revision = "fp16",
     use_auth_token = TOKEN
-)
-
-pipe = pipe.to('cuda')
+).to('cuda')
 
 prompt = "A handsome bunny abasks on the beach"
-image = pipe(f"{prompt}").images[0]  
-image.save(f"{prompt}.png")
+image = pipe(f"{prompt}", num_images_per_prompt = 1).images[0]  
+# image.save(f"{prompt}.png")
 
-ram_cleaning()
+def obtain_image(
+    prompt: str,
+    *,
+    seed: int | None = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5
+) -> Image:
+    generator = None if seed is None else torch.Generator('cuda')
+    print(f"Using device: {pipe.device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator
+    ).images[0]
+    return image
