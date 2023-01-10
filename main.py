f248a83413cb29c975797f031c25a0736f45e915
Andrey <andreypomortsev@gmail.com>
2023-01-10 13:15:03 +0500

    Add get items, generate; post items

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
-
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from ml import obtain_image
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def read_root():
-    return {'Hello':'world'}
+    return {"Hello": "world"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    tags: list[str] = []
+
+@app.post("items/")
+def create_item(item: Item):
+    return item
+
+@app.get("/generate")
+def generate_image(prompt: str):
+    image = obtain_image(prompt, num_inference_steps=5, seed=1024)
+    image.save("image.png")
+    return FileResponse("image.png")
