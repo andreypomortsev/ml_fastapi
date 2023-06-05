460e5eae9c9590a2499644c3a0219c2bf149c65a
Andrey <andreypomortsev@gmail.com>
2023-06-05 14:18:26 +0500

    Refactoring

--- ml.py
+++ ml.py
@@ -6,34 +6,32 @@ from PIL.Image import Image
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 DEVICE = "cuda" if torch.cuda.is_available() else "mps"
 
 model_id = "CompVis/stable-diffusion-v1-4"
 
 scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
 pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
-    scheduler = scheduler,
-    torch_dtype = torch.float16,
-    use_auth_token = TOKEN
+    model_id, scheduler=scheduler, torch_dtype=torch.float16, use_auth_token=TOKEN
 )
 
 pipe = pipe.to(DEVICE)
 
+
 def obtain_image(
     prompt: str,
     *,
-    seed = None,
+    seed=None,
     num_inference_steps: int = 50,
-    guidance_scale: float = 7.5
+    guidance_scale: float = 7.5,
 ) -> Image:
-    generator = None if seed is None else torch.Generator('cuda')
+    generator = None if seed is None else torch.Generator("cuda")
     print(f"Using device: {pipe.device}")
     image: Image = pipe(
         prompt,
         guidance_scale=guidance_scale,
         num_inference_steps=num_inference_steps,
-        generator=generator
+        generator=generator,
     ).images[0]
     return image
